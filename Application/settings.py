828addbb0182877f9dd184fbea99bda7b15a61fa
Tun <lyheanghengcalming@gmail.com>
2025-07-17 09:46:31 +0700

    add views//edit setting,url

--- Application/settings.py
+++ Application/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'Application.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -115,6 +115,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / 'static',]
+STATIC_ROOT = BASE_DIR / 'staticfiles'  # Directory for collected static files
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
--- Application/urls.py
+++ Application/urls.py
@@ -16,7 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.index, name='home'),
+    path('', views.index, name='index'), 
+    path('about/', views.about, name='about'),
+    path('contact/', views.contact, name='contact'),
 ]
